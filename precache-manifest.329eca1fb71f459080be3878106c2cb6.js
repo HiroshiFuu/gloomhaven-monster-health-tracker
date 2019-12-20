8e64695ded6e23ad7c1731ca1d5c46487590474c
Feng Hao <hao.feng@hpe.com>
2019-12-20 10:22:57 +0800

    Updates

---
+++ precache-manifest.329eca1fb71f459080be3878106c2cb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826aca5f54d193831bd04141c967a709",
+    "url": "/gloomhaven-monster-tracker/index.html"
+  },
+  {
+    "revision": "a37841e08f18c843828c",
+    "url": "/gloomhaven-monster-tracker/static/js/2.b74fb5ea.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/gloomhaven-monster-tracker/static/js/2.b74fb5ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de56a22867fea82bf3ab",
+    "url": "/gloomhaven-monster-tracker/static/js/main.af9e63b6.chunk.js"
+  },
+  {
+    "revision": "50518369b350b640ea9f",
+    "url": "/gloomhaven-monster-tracker/static/js/runtime-main.4b7b528d.js"
+  }
+]);
